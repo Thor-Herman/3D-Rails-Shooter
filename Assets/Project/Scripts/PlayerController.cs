78d869f1ffc36198cb506217c6982c74007bc043
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-06-05 17:08:09 +0200

    Add basic movement with clamping to ship

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] private float _xSpeed = 20.0f, _ySpeed = 15.0f, _xRange = 6f, _minYRange = -3f, _maxYRange = 7f;
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        float newXPos = transform.localPosition.x + horizontal * Time.deltaTime * _xSpeed;
+        float newYPos = transform.localPosition.y + vertical * Time.deltaTime * _ySpeed;
+        float clampedXPos = Mathf.Clamp(newXPos, -_xRange, _xRange);
+        float clampedYPos = Mathf.Clamp(newYPos, _minYRange, _maxYRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+
     }
 }
