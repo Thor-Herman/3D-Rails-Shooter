782874cb2a5acb56270c7396bef5d93a28a529cf
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-06-07 20:02:48 +0200

    Add ship pitch, roll, and yaw due to position and input

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float _xSpeed = 20.0f, _ySpeed = 15.0f, _xRange = 6f, _minYRange = -3f, _maxYRange = 7f;
-    // Update is called once per frame
+    [SerializeField]
+    private float _xSpeed = 20.0f, _ySpeed = 15.0f, _xRange = 6f, _minYRange = -3f, _maxYRange = 7f,
+    positionPitchFactor = -3f, controlPitchFactor = -10f, positionYawFactor = 3.5f, controlRollFactor = -13f;
+    float horizontal, vertical;
+
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    void ProcessTranslation()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
         float newXPos = transform.localPosition.x + horizontal * Time.deltaTime * _xSpeed;
         float newYPos = transform.localPosition.y + vertical * Time.deltaTime * _ySpeed;
@@ -17,6 +26,20 @@ public class PlayerController : MonoBehaviour
         float clampedYPos = Mathf.Clamp(newYPos, _minYRange, _maxYRange);
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
 
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = vertical * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+        float yaw = yawDueToPosition;
+
+        float rollDueToControlThrow = horizontal * controlRollFactor;
+        float roll = rollDueToControlThrow;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
