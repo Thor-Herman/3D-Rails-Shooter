8565bcee86b9d56a2379965e0f0964573474480d
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-07-26 22:40:40 +0200

    Add scene reload. Add basic particle explosion to enemy

--- Assets/CollisionHandler.cs
+++ Assets/CollisionHandler.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    // NB! Remember the ship has several colliders. Might lead to bugs in the future. 
-    
-    void OnCollisionEnter(Collision other) {
-        Debug.Log(other.gameObject.name);
-    }
+    [SerializeField] float loadDelay = 1.0f;
 
+    // NB! Remember the ship has several colliders. Might lead to bugs in the future. 
     void OnTriggerEnter(Collider other)
     {
+        StartCoroutine(EndGame());
         Debug.Log(other.gameObject.name);
+    }
+    
+    IEnumerator EndGame() {
+        GetComponent<PlayerController>().enabled = false;
+        yield return new WaitForSeconds(loadDelay);
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
     }
 }
