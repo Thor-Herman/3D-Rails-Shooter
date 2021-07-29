fceda238a565da23af467c5778bcb418baf17054
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-07-29 21:52:21 +0200

    Add explosion on player death

--- Assets/CollisionHandler.cs
+++ Assets/CollisionHandler.cs
@@ -6,6 +6,12 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] float loadDelay = 1.0f;
+    [SerializeField] ParticleSystem crashVFX;
+
+    void Start()
+    {
+        crashVFX.Stop(false);
+    }
 
     // NB! Remember the ship has several colliders. Might lead to bugs in the future. 
     void OnTriggerEnter(Collider other)
@@ -16,6 +22,9 @@ public class CollisionHandler : MonoBehaviour
     
     IEnumerator EndGame() {
         GetComponent<PlayerController>().enabled = false;
+        crashVFX.Play(false);
+        GetComponent<MeshRenderer>().enabled = false;
+        GameObject.Find("PlayerShip/Collider").SetActive(false);
         yield return new WaitForSeconds(loadDelay);
         Scene scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
