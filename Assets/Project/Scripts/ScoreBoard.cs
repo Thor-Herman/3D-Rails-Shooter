142b36f6722f928160677aef5d2cbbb803394636
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-08-01 18:45:03 +0200

    Add UI for showing current score

--- Assets/Project/Scripts/ScoreBoard.cs
+++ Assets/Project/Scripts/ScoreBoard.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     private int _score;
+    private TMP_Text _scoreText;
 
+    void Start()
+    {
+        _scoreText = GetComponent<TMP_Text>();
+    }
+    
     public void ModifyScore(int change) {
         _score += change;
-        Debug.Log(_score);
+        _scoreText.SetText("Score: " + _score.ToString());
     }
 }
