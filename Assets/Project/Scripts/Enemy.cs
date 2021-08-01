f7b4dce83967c29030789d94d4cf714897f1cbc6
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-08-01 20:10:06 +0200

    Add enemy HP and hit material change

--- Assets/Project/Scripts/Enemy.cs
+++ Assets/Project/Scripts/Enemy.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [Tooltip("Material to display when the enemy is hit")][SerializeField] Material hitMaterial;
+    private Material _standardMaterial;
+    private MeshRenderer _meshRenderer;
+    private readonly float _hitDisplayTime = 0.3f;
+
     [Tooltip("How much to increase the player's score")] [SerializeField] private int _scoreIncrementation = 10;
+    [SerializeField] private int _hp = 10;
     private ScoreBoard _scoreBoard;
-    private bool _hasBeenHit = false;
 
     void Start()
     {
         _scoreBoard = FindObjectOfType<ScoreBoard>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _standardMaterial = _meshRenderer.material;
     }
 
     public void OnParticleCollision(GameObject other)
     {
-        if (!_hasBeenHit)
-        {
-            _hasBeenHit = true;
-            _scoreBoard.ModifyScore(_scoreIncrementation);
-            Instantiate(deathVFX, transform.position, Quaternion.identity);
-            Destroy(this.gameObject);
-        }
+        IncreasePlayerScore();
+        _hp--;
+        if (_hp == 0) DestroyShip();
+        else StartCoroutine(PlayHitAnimation());
+    }
+
+    private void IncreasePlayerScore()
+    {
+        _scoreBoard.ModifyScore(_scoreIncrementation);
+    }
+
+    private void DestroyShip() {
+        Instantiate(deathVFX, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+
+    private IEnumerator PlayHitAnimation() {
+        _meshRenderer.material = hitMaterial;
+        yield return new WaitForSeconds(_hitDisplayTime);
+        _meshRenderer.material = _standardMaterial;
     }
 }
