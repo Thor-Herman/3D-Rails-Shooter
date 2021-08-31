fead5fb541abb4a4aa99da12521b6b7997a4c1e3
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-08-31 20:28:36 +0200

    Change crosshair type

--- Assets/Project/Scripts/Crosshair.cs
+++ Assets/Project/Scripts/Crosshair.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Crosshair : MonoBehaviour
 {
-    [Tooltip("The common transform for the two lasers")][SerializeField] Transform laserContainerTransform; // Used instead of ship because of offset.
+    [Tooltip("The transform for the laser")][SerializeField] Transform laserTransform; // Used instead of ship because of offset.
     [SerializeField] Camera cameraObject;
-    [SerializeField] int rayTravelLength = 250;
+    [SerializeField] int rayTravelLength = 100;
 
     void Update()
     {
-        Vector3 rayHit = laserContainerTransform.position + laserContainerTransform.forward * rayTravelLength;
-        Debug.DrawLine(laserContainerTransform.position, rayHit, Color.blue);
+        Vector3 rayHit = laserTransform.position + laserTransform.forward * rayTravelLength;
+        Debug.DrawLine(laserTransform.position, rayHit, Color.blue);
         Vector3 viewPos = cameraObject.WorldToScreenPoint(rayHit);
         transform.position = viewPos;
     }
