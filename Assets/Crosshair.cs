6fd1a91a3f65f03391eac559d84dd5bc738fc951
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-08-31 14:28:25 +0200

    Greatly simplify crosshair code

--- Assets/Crosshair.cs
+++ Assets/Crosshair.cs
@@ -4,29 +4,15 @@ using UnityEngine;
 
 public class Crosshair : MonoBehaviour
 {
-    [SerializeField] Transform playerTransform;
-    [SerializeField] Camera camera;
+    [Tooltip("The common transform for the two lasers")][SerializeField] Transform laserContainerTransform; // Used instead of ship because of offset.
+    [SerializeField] Camera cameraObject;
     [SerializeField] int rayTravelLength = 250;
 
-    Plane plane;
-
-    void Start()
-    {
-        plane = new Plane();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        Vector3 rayHit = playerTransform.position + playerTransform.forward * rayTravelLength;
-        Debug.DrawLine(playerTransform.position, rayHit, Color.red);
-        // plane.SetNormalAndPosition(camera.transform.forward, camera.transform.position);
-        Vector3 planeHit = plane.ClosestPointOnPlane(rayHit);
-        // Vector3 cameraPlane = camera.transform.position + camera.farClipPlane * camera.transform.forward;
-        // Vector3 planeHit = Vector3.ProjectOnPlane(rayHit, camera.transform.position);
-        Debug.DrawLine(camera.transform.position, camera.transform.position + camera.farClipPlane * camera.transform.forward, Color.green);
-        Debug.DrawLine(playerTransform.position, planeHit, Color.blue);
-        Vector3 viewPos = camera.WorldToScreenPoint(planeHit);
+        Vector3 rayHit = laserContainerTransform.position + laserContainerTransform.forward * rayTravelLength;
+        Debug.DrawLine(laserContainerTransform.position, rayHit, Color.blue);
+        Vector3 viewPos = cameraObject.WorldToScreenPoint(rayHit);
         transform.position = viewPos;
     }
 }
